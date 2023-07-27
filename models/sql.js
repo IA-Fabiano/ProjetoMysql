4886fb508c3db5e4b3dd5faaf0db251d50a6d763
Fabiano <Fabiano_carril@hotmail.com>
2023-07-27 16:29:12 -0400

    pre projeto

--- models/sql.js
+++ models/sql.js
@@ -4,7 +4,7 @@ const con = mysql.createConnection({
     host: 'localhost', // O host do banco. Ex: localhost
     user: 'adm', // Um usuário do banco. Ex: user 
     password: '12345', // A senha do usuário. Ex: user123
-    database: 'dblogin' // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
+    database: 'usuarios' // A base de dados a qual a aplicação irá se conectar, deve ser a mesma onde foi executado o Código 1. Ex: node_mysql
 });
 
 //verifica conexao com o banco
